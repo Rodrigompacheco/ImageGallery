791cb125932d3bbb28c497eeb3974c06efd8b0c9
Rodrigo Pacheco <rodrigo.rp.pacheco@gmail.com>
2021-02-26 12:11:01 -0300

    ImageSize struct

---
+++ ImageGallery/ImageGallery/Model/ImageSizeResult.swift
@@ -0,0 +1,8 @@
+//
+//  ImageSizeResult.swift
+//  ImageGallery
+//
+//  Created by Rodrigo Pacheco on 26/02/21.
+//
+
+import Foundation
---
+++ ImageGallery/ImageGallery/Model/PageSizesImageResult.swift
@@ -0,0 +1,8 @@
+//
+//  PageSizesImageResult.swift
+//  ImageGallery
+//
+//  Created by Rodrigo Pacheco on 26/02/21.
+//
+
+import Foundation
